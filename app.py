6c61d71d8f244ebd9c2ce657b945ba486d372164
Richard Gidi <118248129+Richard-Gidi@users.noreply.github.com>
2024-11-30 17:41:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import streamlit as st
 import pandas as pd
 import joblib
 import pickle
-#from google.colab import files
-
-# Upload the model file
-#uploaded = files.upload()
 
 # Load the model from the uploaded file
 model = joblib.load('models/child_mortality_predictor.pkl')
@@ -22,6 +18,17 @@ feature_names = [
     'RCV1 (% of one-year-olds immunized)',
     'DTP3 (% of one-year-olds immunized)'
 ]
+
+# Welcome page introduction
+st.title("Welcome to the Child Mortality Prediction App")
+st.write("""
+### Goal of SDG 3: Good Health and Well-being
+Sustainable Development Goal (SDG) 3 aims to ensure healthy lives and promote well-being for all at all ages. Achieving SDG 3 is vital to addressing the major health challenges facing the world today, from reducing maternal and child mortality rates to tackling communicable diseases such as HIV/AIDS and tuberculosis. It emphasizes the importance of universal health coverage, access to essential health services, and improving health outcomes through investments in healthcare and well-being.
+
+One of the targets within SDG 3 is to reduce child mortality, which remains a significant global issue. Children under the age of five are particularly vulnerable, and efforts are ongoing to ensure that no child dies from preventable causes. This involves improving healthcare systems, increasing vaccination coverage, and providing better access to nutrition and sanitation.
+
+In this app, we aim to support these global efforts by providing a tool to predict child mortality rates based on healthcare indicators. By inputting data such as immunization rates and health insurance coverage, we can gain insights into factors that influence child mortality and work toward better health outcomes.
+""")
 
 # Title of the app
 st.title("Child Mortality Prediction App")
